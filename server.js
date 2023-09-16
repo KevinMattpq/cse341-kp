7301e89d70e93a3ec285047a8da6c4a83d66d205
KevinMattpq <kevinmpena05@gmail.com>
2023-09-16 15:38:44 -0700

    editing route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./Routes')); 
+app.use('/', require('./routes')); 
 
 app.listen(3000, () => {
     console.log(`Server is running on ${port}`);
